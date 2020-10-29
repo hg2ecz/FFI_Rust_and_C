6d7e81d800dabce3bbf385c9907d07f3aab29040
Zsolt Krüpl <hg2ecz@ham.hu>
2020-10-29 07:48:49 +0100

    std::ptr::copy() -> clone_from_slice()

--- cmain_with_rustlib/src/lib.rs
+++ cmain_with_rustlib/src/lib.rs
@@ -14,16 +14,10 @@ pub extern "C" fn vecpow_rs_ffi(
     din_len: i32,
 ) -> i32 {
     let tmp_in = unsafe { std::slice::from_raw_parts(din, din_len as usize) }; // .to_vec();
+    let tmp_out = unsafe { std::slice::from_raw_parts_mut(dout, dout_maxlen as usize) };
+    let tmp_out_vp = vecpow_rs(&tmp_in);
 
-    let tmp_out = vecpow_rs(&tmp_in);
-
-    if dout_maxlen < tmp_out.len() as i32 {
-        panic!("vecpow: len >= out_maxlen");
-    }
-
-    unsafe {
-        std::ptr::copy(tmp_out.as_ptr(), dout, tmp_out.len());
-    }
+    tmp_out.clone_from_slice(&tmp_out_vp);
     tmp_out.len() as i32
 }
 
